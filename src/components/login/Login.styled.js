21983357e6389fa1321eac4072ce0625ef8ff181
ejioguvictor <ejioguvictor6054@gmail.com>
2022-08-01 23:16:22 +0100

    updated the login  styling

--- src/components/login/Login.styled.js
+++ src/components/login/Login.styled.js
@@ -1,11 +1,24 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+padding: 0px;
+`;
+
+export const Nav = styled.nav`
 max-width: 1128px;
 margin: auto;
-padding: 12px 0 16px;
+padding: 12px 0 16px; 
+display: flex;
+position: relative;
+justify-content: space-between;
+flex-wrap: nowrap;
+align-items: center;
 
-`;
-export const Nav = styled.nav`
-background-color: white;
-`
+& > a {
+  width: 135px;
+  height: 34px;
+
+  @media (max-width: 768px) {
+    padding: 0 5px;
+  }
+  } `;
