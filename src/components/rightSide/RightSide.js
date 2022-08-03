ad5c33703ae75ab649872ec7677cbf61e69bc3b5
ejioguvictor <ejioguvictor6054@gmail.com>
2022-08-03 14:03:49 +0100

    updated the right side part if the homepage

--- src/components/rightSide/RightSide.js
+++ src/components/rightSide/RightSide.js
@@ -4,7 +4,44 @@ import { Container, FollowCard, Title, FeedList, Avatar, Recommendation, BannerC
 const RightSide = () => {
   return (
     <Container>
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+          <img src="/images/feed-icon.svg" alt="" />
+        </Title>
 
+        <FeedList>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </FeedList>
+
+        <Recommendation>
+          View all recommendations
+          <img src="/images/right-icon.svg" alt="" />
+        </Recommendation>
+      </FollowCard>
+      <BannerCard>
+        <img
+          src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg"
+          alt=""
+        />
+      </BannerCard>
     </Container>
   );
 };
